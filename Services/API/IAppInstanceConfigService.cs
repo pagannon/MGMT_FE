0cd3a4362678d033ea6f0d65abcc3d4e9d5e95b1
Philip Gannon <philip.gannon@wundermanthompson.com>
2024-11-27 17:11:19 +0100

    The other half of the first working version

---
+++ Services/API/IAppInstanceConfigService.cs
@@ -0,0 +1,23 @@
+﻿namespace OTD.MappingService.ConfigUI.Services.API
+{
+    using OTD.MappingService.Domain.Model.Models.DTOs;
+    using Refit;
+
+    public interface IAppInstanceConfigService
+    {
+        [Get("/api/v1/config/appinstance")]
+        Task<IEnumerable<AppInstance>> GetAll(CancellationToken cancellationToken = default);
+
+        [Get("/api/v1/config/appinstance/{appinstanceId}/")]
+        Task<IEnumerable<AppInstance>> GetById(int appinstanceId, CancellationToken cancellationToken = default);
+
+        [Post("/api/v1/config/appinstance")]
+        Task<IEnumerable<AppInstance>> Create([Body] AppInstance appInstance, CancellationToken cancellationToken = default);
+
+        [Put("/api/v1/config/appinstance/{appinstanceId}/")]
+        Task<IEnumerable<AppInstance>> Update(int appinstanceId, [Body] AppInstance appInstance, CancellationToken cancellationToken = default);
+
+        [Delete("/api/v1/config/appinstance/{appinstanceId}/")]
+        Task<IEnumerable<AppInstance>> Delete(int appinstanceId, CancellationToken cancellationToken = default);
+    }
+}
---
+++ Services/TokenStorageService.cs
@@ -0,0 +1,32 @@
+﻿namespace OTD.MappingService.ConfigUI.Services
+{
+    using Blazored.LocalStorage;
+    using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+
+    public interface ITokenStorageService
+    {
+        Task<AccessToken> GetToken();
+        void SetToken(AccessToken token);
+    }
+
+    public class TokenStorageService : ITokenStorageService
+    {
+        private readonly ILocalStorageService localStorage;
+        private const string storageKey = "TOKENSTORAGE";
+
+        public TokenStorageService(ILocalStorageService LocalStorage)
+        {
+            this.localStorage = LocalStorage;
+        }
+
+        public async void SetToken(AccessToken token)
+        {
+            await this.localStorage.SetItemAsync<AccessToken>(storageKey, token);
+        }
+
+        public async Task<AccessToken?> GetToken()
+        {
+            return await this.localStorage.GetItemAsync<AccessToken>(storageKey);
+        }
+    }
+}
